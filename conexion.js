45d5835f50f2828dde54747aacf5351c4984a175
AgrosoftJL <agrosoftjl@gmail.com>
2026-09-03 23:54:28 -0300

    Compilacion y release v1.0.5 Gordon Gestion Con Sesion

--- conexion.js
+++ conexion.js
@@ -5,6 +5,7 @@ const { createClient } = require('@supabase/supabase-js');
 const SUPABASE_P1_URL = "https://zcmyglespedhcppgxwpg.supabase.co";
 const SUPABASE_P1_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjbXlnbGVzcGVkaGNwcGd4d3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxODI1MTUsImV4cCI6MjA5NTc1ODUxNX0.ouoVoCa5smtHJpTRDMN1dx9dx2qLkoE0qDL5Ug9Dowc";
 
+
 // CONFIGURACIÓN PROYECTO 2: Cosecha, Despachos, Recepción
 const SUPABASE_P2_URL = "https://whiwwfqabpkukamcowbg.supabase.co";
 const SUPABASE_P2_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoaXd3ZnFhYnBrdWthbWNvd2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NTI3MDgsImV4cCI6MjA5MzQyODcwOH0.XK-pOH-LuKOoekko6mAoefd6jxAdk5lUdpeuMyzLde4";
